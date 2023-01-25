9d0514acf3428253abacc3e5e6887a2360e6e90e
Tizu18 <alen_babanovic123@hotmail.com>
2023-01-25 15:07:33 +0100

    bg lines color

--- src/components/shared/PagesHero.tsx
+++ src/components/shared/PagesHero.tsx
@@ -20,13 +20,13 @@ const PagesHero = ({
   imgUrl
 }: Props) => {
   return (
-    <div className="container relative">
+    <div className="container">
       <div className="p-20 bg-gray-primary-alfa relative flex items-center">
         <div className="max-w-[50%]">
           <Title type="h1" text={titleText} highlightText={titleHighlight} />
           {underTitleText && <p className="font-bold mt-9">{underTitleText}</p>}
           <div className="w-10 h-[3px] bg-primary my-5" />
-          <p className="font-normal">{description}</p>
+          <p className="font-normal max-w-[80%]">{description}</p>
         </div>
         <div className="absolute right-0">
           <Image src={imgUrl} alt={imgAlt} />
