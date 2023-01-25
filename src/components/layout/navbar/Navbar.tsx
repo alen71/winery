7d988daa9907f40d9a14853e2cd9d7b820318f1b
Tizu18 <alen_babanovic123@hotmail.com>
2023-01-25 12:24:25 +0100

    toggle test

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -11,9 +11,10 @@ const Navbar = () => {
   const [isScrollTopOfPage, setIsScrollTopOfPage] = useState(true)
   const [open, setOpen] = useState(false)
 
-  // useEffect(() => {
-  //   document.querySelector('html')?.classList.toggle('open')
-  // }, [open])
+  useEffect(() => {
+    document.querySelector('html')?.classList.toggle('overflow-y-scroll')
+    document.querySelector('html')?.classList.toggle('overflow-y-hidden')
+  }, [open])
 
   useEffect(() => {
     let currPageOffset = 0
