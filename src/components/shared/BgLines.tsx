4387fa9f9f69c1b5119aa674252997182907aa75
Tizu18 <alen_babanovic123@hotmail.com>
2023-01-25 12:41:38 +0100

    bg lines opacity

--- src/components/shared/BgLines.tsx
+++ src/components/shared/BgLines.tsx
@@ -7,7 +7,7 @@ const BgLines = () => {
       {Array.from({ length: 5 }, (_, i) => (
         <div
           key={i}
-          className={clsx('h-full w-[1px] bg-gray-light opacity-15', {
+          className={clsx('h-full w-[1px] bg-gray-light opacity-[0.15]', {
             'ml-[-1px]': i === 0,
             'mr-[-1px]': i === 4
           })}
