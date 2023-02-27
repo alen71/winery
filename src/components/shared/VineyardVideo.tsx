2e3d3fe6db17ed8bde8b029685eb85e5a516f106
Tizu18 <alen_babanovic123@hotmail.com>
2023-02-27 20:26:52 +0100

    video test with more formats

--- src/components/shared/VineyardVideo.tsx
+++ src/components/shared/VineyardVideo.tsx
@@ -18,8 +18,17 @@ const VineyardVideo = ({ videoLightOverlay, videoDarkOverlay }: Props) => {
     <>
       {videoLightOverlay && <Overlay videoLight={videoLightOverlay} />}
       {videoDarkOverlay && <Overlay videoDark={videoDarkOverlay} />}
-      <video ref={videRef} className="w-full h-full object-cover">
+      <video
+        ref={videRef}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-full h-full object-cover"
+      >
         <source src="/vinarija-dumo-video.mp4" type="video/mp4" />
+        <source src="/vinarija-dumo-video.webm" type="video/webm" />
+        <source src="/vinarija-dumo-video.ogg" type="video/ogg" />
       </video>
     </>
   )
